034e9c36ed5fc440d2499afb53fcb5ac28375e95
Nayla Taguada <NayTaguada@users.noreply.github.com>
2016-10-28 21:24:01 -0300

    Intentito

---
+++ Practica_Pair_v1.2/Employee.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+    return 0;
+}
+
+
+void employee_print(Employee* this)
+{
+
+}
+
+
+Employee* employee_new(void)
+{
+    Employee* retorno = NULL;
+
+    retorno = malloc(sizeof(Employee));
+
+    /*empleado->id = atoi(id);
+    strcpy(empleado->name,nombre);
+    strcpy(empleado->lastName,apellido);
+    empleado->isEmpty = atoi(estado);
+    */
+    return retorno;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+
+    return 0;
+
+}
+
+int employee_getId(Employee* this)
+{
+
+    return 0;
+
+}
+
+
---
+++ Practica_Pair_v1.2/parser.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
+{
+    //FILE *p;
+    char id[50],nombre[50],apellido[50],estado[50];
+    pFile = fopen("data.csv","r");
+
+    while(!feof(pFile))
+    {
+    Employee *empleado;
+    fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",id,nombre,apellido,estado);
+    printf("%s-%s-%s-%s\n",id,nombre,apellido,estado);
+    empleado = employee_new();
+    empleado->id = atoi(id);
+    strcpy(empleado->name,nombre);
+    strcpy(empleado->lastName,apellido);
+    empleado->isEmpty = atoi(estado);
+        if(al_add(pArrayListEmployee,empleado)==0)
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
+
